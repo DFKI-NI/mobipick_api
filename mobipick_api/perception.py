6acf6a2848680d93c89edf8e279bfabe1b5cc828
Martin Günther <martin.guenther@dfki.de>
2024-07-01 16:36:19 +0200

    Increase sleep while observing to 5s again
    3 seconds was too slow

--- mobipick_api/perception.py
+++ mobipick_api/perception.py
@@ -67,7 +67,7 @@ class Perception:
         resp = self.activate_pose_selector_srv(True)
         rospy.loginfo(f'pose selector response to activation request: {resp}')
         # wait until pose selector gets updates
-        rospy.sleep(3.0)
+        rospy.sleep(5.0)
         # deactivate pose selector detections
         rospy.loginfo('deactivating pose selector')
         resp = self.activate_pose_selector_srv(False)
